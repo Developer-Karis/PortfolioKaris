ec4fa47c0be5fcb5b9c40bf3d21a9bb5fea1d19c
Karis Papadopoulos <60817321+SEVERECLICK-dev@users.noreply.github.com>
2021-02-25 16:21:36 +0100

    change image home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,15 @@
 <section id="hero" class="d-flex align-items-center">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
-                data-aos="fade-up" data-aos-delay="200">
+            <div class="col-lg-6 d-flex flex-column justify-content-center order-2 order-lg-1" data-aos="fade-up"
+                data-aos-delay="200">
                 <h1>I am Karis Papadopoulos <br><span class="type"></span></h1>
                 <div class="d-lg-flex">
                     <a href="#projects" class="btn-get-started scrollto">See my projects</a>
                 </div>
             </div>
             <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                <img src="{{asset('img/hero-img.png')}}" class="img-fluid animated" alt="">
+                <img src="{{asset('img/home.png')}}" class="img-fluid animated" alt="" width="500">
             </div>
         </div>
     </div>
